0442d76f0b0e1d16493c908c5e822435c3c50fc4
Vince Nikolai <153167440+vincenikolai@users.noreply.github.com>
2024-10-10 15:14:46 +0800

    script.js

---
+++ script.js
@@ -0,0 +1,4 @@
+// This is a simple script to show that features like buttons can be interactive
+document.addEventListener('DOMContentLoaded', function () {
+  console.log("Craft & Create site is live!");
+});
